5384936f77e6cf1fcdf557bc421b93b2d00ed550
Michael Rawlings <mlrawlings@embarkweb.com>
2015-01-19 17:27:26 -0500

    v1

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# serve-browser
+# kent-serve-browser
+
+serves js bundles to the browser
+
+## options
+
+### exts
+array of extensions that should be bundled. default: `['js']`
+
+### uglify
+object to pass to [uglify](https://github.com/mishoo/UglifyJS2).
+
+### to5
+object to pass to [6to5](https://github.com/6to5/6to5).  This object can also have two non-standard keys that default to true: `runtime` and `polyfill`.  When true, these include the 6to5 runtime and polyfill scripts, respectively.
+
---
+++ index.js
@@ -0,0 +1,70 @@
+var browserify = require('browserify')
+  , to5ify = require('6to5ify')
+  , envify = require('envify/custom')
+  , uglifyify = require('uglifyify')
+  , defaults = require('defaults')
+  , to5path = './node_modules/6to5ify/node_modules/6to5-core/'
+  , production = process.env.NODE_ENV === 'production'
+
+module.exports = exports = function(root, options) {
+	options = setOptionDefaults(options)
+	
+	return function* (next) {
+		if(!options.exts.test(this.path)) {
+			return yield next
+		}
+
+		this.body = createBundle(root+this.path, options)
+	}
+}
+
+function setOptionDefaults(options) {
+	options = defaults(options, {
+		  exts: ['js']
+	})
+	options.to5 = defaults(options.to5, {
+		  runtime: true
+		, polyfill: true
+	})
+	options.uglify = defaults(options.uglify, {
+		sourcemap: !production
+	})
+	options.exts = extRegex(options.exts)
+	options.vars = getVars()
+	return options
+}
+
+function extRegex(exts) {
+	var srcs = []
+	exts.forEach(function(x) {
+		if(x[0] != '.') x = '.'+x
+		srcs.push('\\'+x+'$')
+	})
+	return new RegExp(srcs.join('|'), 'i')
+}
+
+function createBundle(path, options) {
+	var b = browserify({ debug:!production })
+	
+	if(options.to5.runtime) b.add(to5path+'runtime.js')
+	if(options.to5.polyfill) b.add(to5path+'polyfill.js')
+	
+	b.add(path)
+
+	b.transform(to5ify.configure(options.to5))
+	b.transform(envify(options.vars))
+	b.transform(uglifyify, options.uglify)
+
+	return b.bundle()
+}
+
+function getVars() {
+	var vars = {}
+	Object.keys(process.env).forEach(function(key) {
+		vars[key] = process.env[key]
+	})
+	vars.SERVER = false
+	vars.BROWSER = true
+	vars._ = 'purge'
+	return vars
+}
